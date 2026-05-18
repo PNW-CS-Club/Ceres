98c6c32d119eb42b92e30ba943ca04824ee3ec7e
Gunnar Dahl <73252935+gadahl@users.noreply.github.com>
2026-05-18 15:45:17 -0500

    remove unused files

--- scenes/credits/scrollable_credits.gd
+++ scenes/credits/scrollable_credits.gd
@@ -10,7 +10,6 @@ var _line_number : float = 0
 func _on_visibility_changed() -> void:
 	if visible:
 		credits_label.scroll_to_line(0)
-		credits_label.grab_focus()
 
 func _ready() -> void:
 	visibility_changed.connect(_on_visibility_changed)
